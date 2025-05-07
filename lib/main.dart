86c9ec0910cfdf4db330efde0dfff1c42e019650
Anadudev <anadudev@gmail.com>
2025-05-07 11:58:15 +0100

    Initial project setup

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: unnecessary_null_comparison, prefer_const_constructors
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone/cores/firebase_options.dart';
+import 'package:youtube_clone/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
